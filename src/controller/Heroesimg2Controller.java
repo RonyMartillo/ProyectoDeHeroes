c01c2603c1654f9f86443d8cda955869bde88312
RonyMartillo <ray129091@gmail.com>
2022-02-21 16:11:03 -0500

    Creacion de 2 pantallas para visualizar imagenes de los heroes

---
+++ src/controller/Heroesimg2Controller.java
@@ -0,0 +1,23 @@
+
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Shokuhou
+ */
+public class Heroesimg2Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/controller/Heroesimg3Controller.java
@@ -0,0 +1,49 @@
+
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Shokuhou
+ */
+public class Heroesimg3Controller implements Initializable {
+
+    @FXML
+    private ImageView img1;
+    @FXML
+    private ImageView imag2;
+    @FXML
+    private ImageView imag3;
+    @FXML
+    private ImageView imag4;
+    @FXML
+    private ImageView img5;
+    @FXML
+    private ImageView img6;
+    @FXML
+    private ImageView img7;
+    @FXML
+    private ImageView img8;
+    @FXML
+    private ImageView img9;
+    @FXML
+    private ImageView img10;
+    @FXML
+    private Label lbltext;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
