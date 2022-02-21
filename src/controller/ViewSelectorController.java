8db4005dfad4f4aa8e9365acb5c1b9b34071c96e
JoseChavarria96 <josepato14@hotmail.com>
2022-02-20 23:12:28 -0500

    creacion de dos pantallas

---
+++ src/controller/ViewSelectorController.java
@@ -0,0 +1,74 @@
+
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import util.Conexion;
+
+/**
+ * FXML Controller class
+ *
+ * @author Shokuhou
+ */
+public class ViewSelectorController implements Initializable {
+
+    @FXML
+    private Button btnMarvel;
+    @FXML
+    private Button btnDC;
+    @FXML
+    private Button btnEditor;
+    @FXML
+    private Button btnBuscar;
+
+    Conexion con = new Conexion();
+    @FXML
+    private Button btnSearchFill;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void mostrarMarvel(ActionEvent event) throws Exception {
+
+     
+    }
+
+    @FXML
+    private void buscar(ActionEvent event) throws Exception {
+
+        
+
+    }
+
+    @FXML
+    private void mostrardc(ActionEvent event) throws IOException {
+      
+    }
+
+    @FXML
+    private void editor(ActionEvent event) throws IOException {
+       }
+    
+
+    @FXML
+    private void onSeachFill(ActionEvent event) throws IOException {
+       
+
+}
---
+++ src/controller/ViewUserController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author JESUS
+ */
+public class ViewUserController implements Initializable {
+
+    @FXML
+    private Button btnMarvel;
+    @FXML
+    private Button btnDC;
+    @FXML
+    private Button btnBuscar;
+    @FXML
+    private Button btnSearchAll;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void mostrarMarvel(ActionEvent event) {
+      
+    }
+
+    @FXML
+    private void mostrardc(ActionEvent event) {
+       
+    }
+
+    @FXML
+    private void buscar(ActionEvent event) {
+
+       
+
+    }
+
+    @FXML
+    private void searchAll(ActionEvent event) {
+      
+    }
+}
